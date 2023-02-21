c13b51470ef40be5ab7f50e347bebc68bd0b4246
Neto test <kimirina0676@mail.ru>
2023-02-21 11:28:08 +0300

    Radio init

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,27 +1,74 @@
 
 public class Radio {
-    private int currentStation;
+    private int maxVolume = 10;
+    private int minVolume = 0;
+    private int maxChannel = 9;
+    private int minChannel = 0;
     private int currentVolume;
+    private int currentChannel;
 
-    public int getCurrentStation() {
-        return currentStation;
+    public void increaseVolume() {
+        if (currentVolume == maxVolume) {
+            return;
+        }
+        currentVolume++;
     }
+
+    public void decreaseVolume() {
+        if (currentVolume == minVolume) {
+            return;
+        }
+        currentVolume--;
+    }
+
+    public void increaseChannel() {
+        if (currentChannel == maxChannel) {
+            this.currentChannel = minChannel;
+            return;
+        }
+        currentChannel++;
+    }
+
+    public void decreaseChannel() {
+        if (currentChannel == minChannel) {
+            this.currentChannel = maxChannel;
+            return;
+        }
+        currentChannel--;
+    }
+
 
     public int getCurrentVolume() {
         return currentVolume;
     }
 
-    public void setCurrentStation(int newCurrentStation) {
-        if (newCurrentStation < 0) {
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > maxVolume) {
+            this.currentVolume = maxVolume;
             return;
         }
-        if (newCurrentStation > 9) {
+        if (currentVolume < minVolume) {
+            this.currentVolume = minVolume;
             return;
         }
-        currentStation = newCurrentStation;
+        this.currentVolume = currentVolume;
     }
 
-    public void setNextStation() {
 
+    public void setCurrentChannel(int currentChannel) {
+        if (currentChannel > maxChannel) {
+
+            return;
+        }
+        if (currentChannel < minChannel) {
+
+            return;
+        }
+        this.currentChannel = currentChannel;
+    }
+
+    public int getCurrentChannel() {
+        return currentChannel;
     }
 }
+
